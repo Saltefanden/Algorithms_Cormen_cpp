ed76b0873be1d8af9d8ffef48e7b40092fc1dfad
Nicklas@privateComputer <Saltefanden@github.com>
2022-05-18 21:58:13 +0200

    BUGFIX: Makes reverse insertion sort actually reverse and updates tests to test for it

--- sorting/insertion_sort/src/reverse_insertion_sort.cpp
+++ sorting/insertion_sort/src/reverse_insertion_sort.cpp
@@ -4,7 +4,7 @@ void reverse_insertion_sort(int* arr, int size){
   for (int j{1}; j<size; ++j){
     key = arr[j];
     i = j-1;
-    while (i>=0 && arr[i]>key){
+    while (i>=0 && arr[i]<key){
       arr[i+1] = arr[i];
       --i;
     }
--- sorting/insertion_sort/tests/test_reverse_insertion_sort.cpp
+++ sorting/insertion_sort/tests/test_reverse_insertion_sort.cpp
@@ -1,8 +1,22 @@
 #include<gtest/gtest.h>
-#include<insertion_sort.hpp>
+#include<reverse_insertion_sort.hpp>
 #include<vector>
 #include<algorithm>
 
 TEST(Base, Smoketest){
   EXPECT_EQ(1,1);
+}
+
+TEST(CArray, SortEasyImproved){
+  int arr[]{ 0, 2, 45, 10, 4, 6 };
+  size_t arr_size{sizeof(arr)/sizeof(arr[0])};
+
+  std::vector<int> vec(arr, arr + arr_size);
+  std::sort(vec.begin(), vec.end(), std::greater());
+
+  reverse_insertion_sort(arr, arr_size);
+
+  for (size_t i=0; i<arr_size; ++i){
+    EXPECT_EQ(arr[i], vec[i]);
+  }
 }

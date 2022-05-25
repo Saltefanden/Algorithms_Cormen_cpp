998ec2009917fd69f225a8c81e98af1589814f61
Nicklas NNIT <nimu@nnit.com>
2022-05-25 16:45:59 +0200

    Adds complicated unreadable mergesort

--- sorting/merge_sort/src/merge.cpp
+++ sorting/merge_sort/src/merge.cpp
@@ -1,5 +1,35 @@
 void merge(int* arr, int leftStart, int leftEnd, int rightEnd){
 
-  int size_left{leftEnd - leftStart};
+  int leftSize{leftEnd - leftStart};
+  int rightSize{rightEnd - leftEnd};
+
+  int leftArr[leftSize];
+  for (int i{}; i<leftSize; ++i){
+    leftArr[i] = arr[i+leftStart];
+  }
+
+  int rightArr[rightSize];
+  for (int i{}; i<rightSize; ++i){
+    rightArr[i] = arr[i+leftEnd];
+  }
+
+  int li{0};
+  int ri{0};
+  for (int i{leftStart}; i<rightEnd; ++i){
+    if (li < leftSize && ri < rightSize){
+      leftArr[li] <= rightArr[ri] ? ((arr[i] = leftArr[li]), ++li) : ( (arr[i] = rightArr[ri]), ++ri); 
+    } else if (li == leftSize && ri == rightSize){
+      return;
+    } else if (li == leftSize) {
+      arr[i] = rightArr[ri];
+      ++ri;
+    } else if (ri == rightSize) {
+      arr[i] = leftArr[li];
+      ++li;
+    } else {
+      arr = nullptr; // here be dragons
+    }
+
+  }
 
 }
--- sorting/merge_sort/tests/test_merge.cpp
+++ sorting/merge_sort/tests/test_merge.cpp
@@ -7,16 +7,25 @@ TEST(Basic, Smoketest){
   EXPECT_TRUE(true);
 }
 
+TEST(Language, CommaAssigning){
+  int arr[]{1,2,3, 0, 2, 4};
+  int p = 2;
+  (arr[0] = arr[p]), ++p;
+  EXPECT_EQ(arr[0], 3);
+  EXPECT_EQ(arr[p], 0);
+}
+
+
 TEST(Merging, SimpleAppend){
   int arr[]{1,2,3, 0, 2, 4};
   size_t arr_size{sizeof(arr)/sizeof(arr[0])};
   std::vector<int> vec(arr, arr + arr_size);
 
-  merge(arr, 0, 2, 5);
-  std::sort(vec.begin(), vec.end());
+  merge(arr, 1, 3, arr_size-1);
+  std::sort(vec.begin()+1, vec.end()-1);
 
   for (int i{0}; i<6; ++i){
-    EXPECT_EQ(arr[i], vec[i]); 
+    EXPECT_EQ(arr[i], vec[i]) << " with i="<<i; 
   } 
 
 }

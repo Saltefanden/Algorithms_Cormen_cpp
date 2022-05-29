6510675fc44362b44d58720c6796780cbc85a80d
Nicklas@privateComputer <Saltefanden@github.com>
2022-05-29 18:03:12 +0200

    Improves merge for readability and extensibility

--- sorting/merge_sort/src/merge.cpp
+++ sorting/merge_sort/src/merge.cpp
@@ -1,42 +1,26 @@
-void merge(int* arr, int leftStart, int leftEnd, int rightEnd){
+void merge(int arr[], int startidx, int mididx, int endidx){
 
-  int leftSize{leftEnd - leftStart};
-  int rightSize{rightEnd - leftEnd};
+  int arrSize{endidx - startidx};
+  int copyArr[arrSize];
 
-  int leftArr[leftSize];
-  // int* leftArr = new int[leftSize];
-  for (int i{}; i<leftSize; ++i){
-    leftArr[i] = arr[i+leftStart];
+  for (int i{}; i<arrSize; ++i){
+    copyArr[i] = arr[startidx+i];
   }
 
-  int rightArr[rightSize];
-  // int* rightArr = new int[rightSize];
-  for (int i{}; i<rightSize; ++i){
-    rightArr[i] = arr[i+leftEnd];
-  }
+  int copyStartidx{startidx - startidx};
+  int copyMididx{mididx - startidx};
+  int copyEndidx{endidx - startidx};
 
-  int li{0};
-  int ri{0};
-  int i{leftStart};
-  while (true){
-    if (li < leftSize && ri < rightSize){
-      leftArr[li] <= rightArr[ri] ? ((arr[i] = leftArr[li]), ++li) : ( (arr[i] = rightArr[ri]), ++ri); 
-    } else if (li == leftSize && ri == rightSize){
-      // delete[] leftArr;
-      // delete[] rightArr;
-      return;
-    } else if (li == leftSize) {
-      arr[i] = rightArr[ri];
-      ++ri;
-    } else if (ri == rightSize && li != leftSize) {
-      arr[i] = leftArr[li];
-      ++li;
-    } else {
-      // delete[] leftArr;
-      // delete[] rightArr;
-      arr = nullptr; // here be dragons
-    }
-    ++i;
+  int li{copyStartidx};
+  int ri{copyMididx};
+  int i{startidx};
+  while (li < copyMididx && ri < copyEndidx){
+      copyArr[li] <= copyArr[ri] ? (arr[i++] = copyArr[li++]) : (arr[i++] = copyArr[ri++]);
   }
-
+  while (li < copyMididx){
+    arr[i++] = copyArr[li++];
+  }
+  while (ri < copyEndidx){
+    arr[i++] = copyArr[ri++];
+  }
 }
